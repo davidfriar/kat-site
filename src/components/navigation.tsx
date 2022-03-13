fc8b44618a921cdd099ce13b1ad9c1d13e4b07db
David Friar <david.friar@cognifide.com>
2022-03-13 13:04:15 +0000

    fix for menu not closing after adding gatsby links

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,21 +1,41 @@
 import { useNavigation } from "../hooks/useNavigation"
 import { Link } from "gatsby"
+import { useState } from "react"
 
 const Navigation = () => {
   const navigation = useNavigation(false)
+  const [open, setOpen] = useState(false)
   return (
     <>
-      <input type="checkbox" className="nav-toggle" id="nav-toggle" />
+      <input
+        type="checkbox"
+        className="nav-toggle"
+        checked={open}
+        id="nav-toggle"
+      />
       <nav>
         <ul>
           {navigation.map((page) => (
             <li key={page.title}>
-              <Link to={page.url}>{page.title}</Link>
+              <Link
+                to={page.url}
+                onClick={() => {
+                  setOpen(false)
+                }}
+              >
+                {page.title}
+              </Link>
             </li>
           ))}
         </ul>
       </nav>
-      <label htmlFor="nav-toggle" className="nav-toggle-label">
+      <label
+        htmlFor="nav-toggle"
+        onClick={() => {
+          setOpen((open) => !open)
+        }}
+        className="nav-toggle-label"
+      >
         <span></span>
       </label>
     </>
