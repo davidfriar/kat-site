d59ce8141ae6882166123848339c7505c0aab47f
David Friar <david.friar@cognifide.com>
2022-03-13 15:26:49 +0000

    hack to prevent coverflow opening new tab

--- src/templates/coverflow.tsx
+++ src/templates/coverflow.tsx
@@ -2,10 +2,19 @@ import { SanityPost } from "../../graphql-types"
 import Image from "../components/image"
 import ReactCoverflow from "react-coverflow"
 import "./coverflow.css"
+import { navigate } from "gatsby"
 
 type CoverflowProps = { posts: SanityPost[] }
 
 const Coverflow = ({ posts }: CoverflowProps) => {
+  const myopen = (url?: string | URL): null => {
+    navigate(url as string)
+    return null
+  }
+  if (window) {
+    window.open = myopen
+  }
+
   return (
     <div className="coverflow">
       <ReactCoverflow
